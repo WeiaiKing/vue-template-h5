6e35bb6b10cd85e87348bea84a5caaee99d2d81d
韦加金 <1192620781@qq.com>
2020-12-16 23:13:38 +0800

    feat: 登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  // 重定向
   {
     path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: () => import('../views/Home.vue'),
     redirect: '/index',
@@ -32,6 +42,19 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  /**
+   * to 将要访问的路径
+   * from 代表从哪个路径跳转而来
+   * next 是一个函数，表示放行
+   *  next() 放行 next('/login') 强制跳转
+   * */
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
